5d27437ee9bedd9e2fc4f29d330a9861b2053782
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-02-24 12:51:14 -0300

    Criando a configuração inicial da aplicação base

--- main/next.config.ts
+++ main/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
+const USERS_URL = process.env.USERS_URL;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    if (!USERS_URL) return [];
+
+    return [
+      {
+        source: "/users",
+        destination: `${USERS_URL}/users`,
+      },
+      {
+        source: "/users/:path*",
+        destination: `${USERS_URL}/users/:path*`,
+      },
+
+      // Assets do Next (obrigatório)
+      {
+        source: "/users/_next/:path*",
+        destination: `${USERS_URL}/_next/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
